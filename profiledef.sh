408446b94f74c091544fc85b17dfee9d765b1c4f
joviqa <joviqa@hotmail.com>
2021-11-05 16:41:29 -0500

    [v0.4c] fix office and to server (testing)

--- profiledef.sh
+++ profiledef.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="lupaca"
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux Lupaca  <https://archlinux.org>"
-iso_application="Arch Linux Lupaca Live"
+iso_name="ukhampuni"
+iso_label="UI_$(date +%Y%m)"
+iso_publisher="Ukhampuni  <https://ukhampuni.org.pe>"
+iso_application="Ukhampuni Live"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
+#airootfs_image_tool_options=('-comp' 'xz')
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
-  ["/root/.automated_script.sh"]="0:0:755"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
+  #["/root/.automated_script.sh"]="0:0:755"
+  #["/usr/local/bin/choose-mirror"]="0:0:755"
+  #["/usr/local/bin/Installation_guide"]="0:0:755"
+  #["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/local/bin/lupaca-before"]="0:0:755"
+  ["/usr/local/bin/lupaca-final"]="0:0:755"
   ["/etc/sudoers.d"]="0:0:750"
   ["/etc/shadow"]="0:0:0400"
   ["/etc/gshadow"]="0:0:0400"
-  ["/etc/os-release"]="0:0:777"
+  ["/home/liveuser/Desktop/calamares.desktop"]="0:0:755"
   ["/etc/vconsole.conf"]="0:0:644" 
 )
